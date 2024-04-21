2081d5334f8b03f2e382c88ed6f52c826c0fe69c
kossy205 <kosisochukwuhillary45@gmail.com>
2024-04-21 18:24:25 +0100

    save run to local db

--- app/src/main/java/com/example/runtracker/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/runtracker/ui/viewmodels/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.runtracker.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.runtracker.db.Run
 import com.example.runtracker.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +13,7 @@ class MainViewModel @Inject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
 
+    fun insert(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
