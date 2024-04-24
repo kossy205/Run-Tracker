f7cb1163cc02d236280dde725a92c502b0a141f8
kossy205 <kosisochukwuhillary45@gmail.com>
2024-04-24 23:34:31 +0100

    Saving Username and Weight

--- app/src/main/java/com/example/runtracker/di/AppModule.kt
+++ app/src/main/java/com/example/runtracker/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.example.runtracker.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.example.runtracker.others.Constants.RUNNING_DATABASE_NAME
 import com.example.runtracker.db.RunDatabase
+import com.example.runtracker.others.Constants.KEY_FIRST_TIME_TOGGLE
+import com.example.runtracker.others.Constants.KEY_NAME
+import com.example.runtracker.others.Constants.KEY_WEIGHT
+import com.example.runtracker.others.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,4 +35,23 @@ object AppModule {
     @Singleton
     @Provides
     fun provideRunDao(db: RunDatabase) = db.getRunDao()
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPref: SharedPreferences) = sharedPref.getString(KEY_NAME, "") ?: ""
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPref: SharedPreferences) = sharedPref.getFloat(KEY_WEIGHT, 80f)
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPref: SharedPreferences) =
+        sharedPref.getBoolean(KEY_FIRST_TIME_TOGGLE, true)
 }
--- app/src/main/java/com/example/runtracker/ui/fragments/SetUpFragment.kt
+++ app/src/main/java/com/example/runtracker/ui/fragments/SetUpFragment.kt
@@ -1,32 +1,94 @@
 package com.example.runtracker.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.runtracker.R
 import com.example.runtracker.databinding.FragmentSetupBinding
+import com.example.runtracker.others.Constants.KEY_FIRST_TIME_TOGGLE
+import com.example.runtracker.others.Constants.KEY_NAME
+import com.example.runtracker.others.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
+import javax.inject.Inject
 
 class SetUpFragment: Fragment(R.layout.fragment_setup) {
 
     private lateinit var binding: FragmentSetupBinding
 
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
+    @set:Inject
+    var isFirstTimeOpen = true
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        if(!isFirstTimeOpen){
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.setUpFragment, true)
+                .build()
+            findNavController().navigate(
+                R.id.action_setUpFragment_to_runFragment,
+                savedInstanceState,
+                navOptions
+            )
+        }
+
         // Initialize the binding variable
         binding = FragmentSetupBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnContinue.setOnClickListener {
-            findNavController().navigate(R.id.action_setUpFragment_to_runFragment)
+            val success = writePersonalDataToSharedPref()
+            if(success){
+                findNavController().navigate(R.id.action_setUpFragment_to_runFragment)
+            }else{
+                Snackbar.make(requireView(), "Please enter all fields", Snackbar.LENGTH_SHORT).show()
+            }
         }
     }
+
+    private fun writePersonalDataToSharedPref(): Boolean{
+        val name = binding.etName.text.toString()
+        val weight = binding.etWeight.text.toString()
+        if(name.isEmpty() || weight.isEmpty()){
+            return false
+        }
+        sharedPref.edit()
+            .putString(KEY_NAME, name)
+            .putFloat(KEY_WEIGHT, weight.toFloat())
+            .putBoolean(KEY_FIRST_TIME_TOGGLE, false)
+            .apply()
+
+        val toolBarText = "let's go $name"
+
+
+        return true
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
